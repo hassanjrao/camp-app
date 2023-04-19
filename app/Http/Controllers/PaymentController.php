088f7fd2b15bfcdfd8a54fe55d7aa29086f46f85
hassan <hassan@thiqatech.com>
2023-04-19 05:44:30 +0400

    working on square

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,7 +8,7 @@ use Nikolag\Square\Facades\Square;
 class PaymentController extends Controller
 {
 
-    public function storeO(Request $request)
+    public function store(Request $request)
     {
         // $request->validate([
         //     'amount' => 'required|numeric',
@@ -75,7 +75,7 @@ class PaymentController extends Controller
 
     }
 
-    public function store(Request $request)
+    public function stores(Request $request)
     {
 
         $amount = 5000; //Is in USD currency and is in smallest denomination (cents). ($amount = 5000 == 50 Dollars)
