707791bc0ee3624a04b91f61c8757c4eb5f35ba6
hassan <hassan@thiqatech.com>
2023-04-19 04:56:03 +0400

    working on square

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,38 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Nikolag\Square\Facades\Square;
 
 class PaymentController extends Controller
 {
 
     public function store(Request $request)
     {
-        dd($request->all());
+        // $request->validate([
+        //     'amount' => 'required|numeric',
+        //     'nonce' => 'required|string',
+        // ]);
 
-        $square_client = new SquareClient([
-            'accessToken' => $access_token,
-            'environment' => getenv('ENVIRONMENT')
-        ]);
+        //$amount is in USD currency and is in smallest denomination (cents). ($amount = 200 == 2 Dollars)
+        $amount = 1000;
 
-        $payments_api = $square_client->getPaymentsApi();
+        //nonce reference => https://docs.connect.squareup.com/articles/adding-payment-form
+        $formNonce = $request->token;
 
-        $money = new Money();
-        $money->setAmount(100);
-        // Set currency to the currency for the location
-        $money->setCurrency($location_info->getCurrency());
+        //$location_id is id of a location from Square
+        $location_id = env('SQUARE_LOCATION_ID');
 
-        // Every payment you process with the SDK must have a unique idempotency key.
-        // If you're unsure whether a particular payment succeeded, you can reattempt
-        // it with the same idempotency key without worrying about double charging
-        // the buyer.
-        $create_payment_request = new CreatePaymentRequest($token, Uuid::uuid4(), $money);
+        //note about this order
+        $note = 'My first order made with Square';
 
-        $response = $payments_api->createPayment($create_payment_request);
+        //some kind of reference id to an object or resource
+        $reference_id = 'client_1_bought_file.pdf';
 
-        if ($response->isSuccess()) {
-            echo json_encode($response->getResult());
-        } else {
-            echo json_encode($response->getErrors());
-        }
+        $options = array(
+            'amount' => $amount,
+            'source_id' => $formNonce,
+            'location_id' => $location_id,
+            'note' => $note,
+            'reference_id' => $reference_id
+        );
+
+        $products = array(
+            [
+                'name' => 'Shirt',
+                'variation_name' => 'Large white',
+                'note' => 'This note can have maximum of 50 characters.',
+                'price' => 440.99,
+                'quantity' => 2,
+                'reference_id' => '5' //An optional ID to associate the product with an entity ID in your own table
+            ],
+            [
+                'name' => 'Shirt',
+                'variation_name' => 'Mid-size yellow',
+                'note' => 'This note can have maximum of 50 characters.',
+                'quantity' => 1,
+                'price' => 118.02
+            ],
+        );
+
+        $discount = array(
+            'name' => 'Sale Mania -5 USD',
+            'amount' => 500 //It is in the smallest denomination of currency provided (if currency = USD then amount = 500 cents == 5 USD)
+        );
+
+        $order = array(
+            'products' => $products
+        );
+
+
+        //Create and save order and apply a discount to the order
+        $order['discounts'] = array($discount);
+        $square = Square::setOrder($order, $location_id)->save();
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nikolag\Square\Traits\HasProducts;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, HasProducts;
+
+    protected $table='orders';
 }
